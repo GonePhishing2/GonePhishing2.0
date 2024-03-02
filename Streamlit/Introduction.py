7ab112fd564a1a5798b658fedfc4d1af80cdd3e9
Logan Ray <104516932+loganray48@users.noreply.github.com>
2024-03-02 11:33:15 -0700

    Update Introduction.py

--- Streamlit/Introduction.py
+++ Streamlit/Introduction.py
@@ -1,38 +1,26 @@
 import streamlit as st
 
 # Set the title of the app
-st.title('Koch Industries Report')
+st.title('Project Outline/Background')
 
 # Sidebar for navigation with HTML and inline CSS to mimic normal text appearance
 st.sidebar.title('Navigation')
 st.sidebar.markdown('<a href="#project-outline" style="text-decoration: none; color: black;">Project Outline</a>', unsafe_allow_html=True)
 st.sidebar.markdown('<a href="#problem-statement" style="text-decoration: none; color: black;">Problem Statement</a>', unsafe_allow_html=True)
-st.sidebar.markdown('<a href="#data-preparationcleaning" style="text-decoration: none; color: black;">Data Preparation/Cleaning</a>', unsafe_allow_html=True)
-st.sidebar.markdown('<a href="#feature-engineering" style="text-decoration: none; color: black;">Feature Engineering</a>', unsafe_allow_html=True)
-st.sidebar.markdown('<a href="#modeling-techniques" style="text-decoration: none; color: black;">Modeling Techniques</a>', unsafe_allow_html=True)
-st.sidebar.markdown('<a href="#criteria-for-choosing-a-model" style="text-decoration: none; color: black;">Criteria for Choosing a Model</a>', unsafe_allow_html=True)
+
+
 
 # Sections of the app
 st.markdown("<a name='project-outline'></a>", unsafe_allow_html=True)
 st.header('Collaboratively Outline the Project')
-st.text('Insert your content here...')
+st.write("This project represents a concerted effort by our team to tackle Koch Industries pressing issue of parts delivery delays. \
+        We're methodically working through stages from data cleaning to model selection, aiming to significantly improve our prediction accuracy. \
+        By transforming disjointed data into a reliable forecasting tool, we intend to provide Koch with the ability to anticipate delays and plan accordingly, thus maintaining uninterrupted production and safeguarding the bottom line.")
 
 st.markdown("<a name='problem-statement'></a>", unsafe_allow_html=True)
 st.header('Problem Statement')
-st.text('Insert your content here...')
+st.write('Insert your content here...')
 
-st.markdown("<a name='data-preparationcleaning'></a>", unsafe_allow_html=True)
-st.header('Data Preparation/Cleaning')
-st.text('Insert your content here...')
 
-st.markdown("<a name='feature-engineering'></a>", unsafe_allow_html=True)
-st.header('Feature Engineering')
-st.text('Insert your content here...')
 
-st.markdown("<a name='modeling-techniques'></a>", unsafe_allow_html=True)
-st.header('Modeling Techniques')
-st.text('Insert your content here...')
 
-st.markdown("<a name='criteria-for-choosing-a-model'></a>", unsafe_allow_html=True)
-st.header('Criteria for Choosing a Model')
-st.text('Insert your content here...')
