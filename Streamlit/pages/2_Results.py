176cd27dabf5042a3d68fc623c4f4613428b22a3
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-03-02 15:10:44 -0800

    Update 2_Results.py

--- Streamlit/pages/2_Results.py
+++ Streamlit/pages/2_Results.py
@@ -12,12 +12,17 @@ st.markdown("<a name='criteria-for-choosing-a-model'></a>", unsafe_allow_html=Tr
 st.header('Criteria for Choosing a Model')
 st.write('Insert your content here...')
 
+st.subheader("Features")
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/hackusu_scatterplot.png', caption='scatter')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/hackusu_scatterplot_code.png', caption='scatter_code')
 
+st.subheader("Binary Model")
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/binary_models.png', caption='binary_model')
+
+st.subheader("Basic Regression")
+
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_feature.png', caption='class_feature')
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_fold.png', caption='class_fold')
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_model.png', caption='class_model')
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_recall_curve.png', caption='class_recall')
 st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_roc.png', caption='class_roc')
-st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/hackusu_scatterplot.png', caption='scatter')
-st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/hackusu_scatterplot_code.png', caption='scatter_code')
