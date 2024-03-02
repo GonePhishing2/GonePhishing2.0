e5af0cee65f462578e3731da8864ef083202d8d2
loganray <loganray4898@gmail.com>
2024-03-02 16:05:13 -0700

    image_update

--- Streamlit/pages/2_Results.py
+++ Streamlit/pages/2_Results.py
@@ -11,3 +11,11 @@ st.sidebar.markdown('<a href="#criteria-for-choosing-a-model" style="text-decora
 st.markdown("<a name='criteria-for-choosing-a-model'></a>", unsafe_allow_html=True)
 st.header('Criteria for Choosing a Model')
 st.write('Insert your content here...')
+
+
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/binary_models.png', caption='binary_model')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_feature.png', caption='class_feature')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_fold.png', caption='class_fold')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_model.png', caption='class_model')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_recall_curve.png', caption='class_recall')
+st.image('https://raw.githubusercontent.com/peircerandy/GonePhishing2.0/main/Streamlit/images/classification_roc.png', caption='class_roc')
